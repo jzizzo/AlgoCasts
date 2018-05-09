bc94f3effe8cf8e950eaad3c054a4a86eed313e9
Joseph Zizzo <Joseph.Zizzo@logmein.com>
2018-05-09 16:48:10 -0700

    Add reverseString solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  // create result
+  let reversedStr = '';
+  // iterate through string starting at end towards start
+  for (let i = str.length - 1; i >= 0 ; i--) {
+      // add our current letter to the result
+      reversedStr += str[i];
+      debugger;
+    }
+    // return result
+    return reversedStr;
+
+    // return str.split('').reverse().join('');
+
+    // return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+// You have to invoke the funciton in order to use node inspect index.js and then repl
+// reverse('asdf');
 
 module.exports = reverse;
