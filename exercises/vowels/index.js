d62701ee0323900375d86136b738ca82bdc663a0
Joseph Zizzo <hire.jzizzo@gmail.com>
2018-12-18 18:03:09 -0800

    Add vowels solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+}
+
+
+// // quick, first solution:
+// function vowels(str) {
+//   // set count
+//   let count = 0;
+//   const vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+
+//   // iterate through vowels
+//   // edge case: casing
+//   for (let letter of str.toLowerCase()) {
+//     // is current letter a vowel?
+//     if (vowelCheck.includes(letter)) {
+//       // yes, inc count
+//       count++;
+//     }
+//   }
+//   // return count
+//   return count;
+// }
 
 module.exports = vowels;
