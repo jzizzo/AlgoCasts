282ad57f3503e631cb15715d945860f28f555a33
Joseph Zizzo <hire.jzizzo@gmail.com>
2018-12-19 11:20:43 -0800

    Add fib solutions

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,44 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// memo:
+  function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      if(cache[args]) {
+        return cache[args];
+      }
+
+      const result = fn.apply(this, args);
+      cache[args] = result;
+
+      return result;
+    };
+  }
+
+// recursive: 
+function slowFib(n) {
+  if (n < 2){
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+}
+
+const fib = memoize(slowFib);
+
+// function fib(n) {
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[n];
+// }
+
 
 module.exports = fib;
